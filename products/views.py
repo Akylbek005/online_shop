ae8ba77ebd28fb89069d3f00bdd930bfe56e06f0
My name <iakylbek005@gmail.com>
2021-07-08 11:09:41 +0600

    Use ModelViewSet

--- products/views.py
+++ products/views.py
@@ -1,7 +1,4 @@
-from rest_framework import status, generics, viewsets
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import CreateModelMixin, ListModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework import viewsets
 
 from .serializer import ProductsSerializer
 from .models import Products
