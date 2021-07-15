d28974e10b4b50ba3582926df5c94c20abaa297f
My name <iakylbek005@gmail.com>
2021-07-15 15:05:54 +0600

    permissions

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-
+from rest_framework import permissions
 from .serializer import ProductsSerializer
 from .models import Products
 
@@ -7,3 +7,4 @@ from .models import Products
 class ProductView(viewsets.ModelViewSet):
     serializer_class = ProductsSerializer
     queryset = Products.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
